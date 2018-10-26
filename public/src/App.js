5adbb9aae1c211f4739d4b179e3a5bfe6ebd0a4b
Matteo Manzinello <matteo@manzinello.com>
2018-10-26 17:12:19 +0200

    Aggiustato css, aggiunto svg di react

--- public/src/App.js
+++ public/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import { Icon } from "antd";
 
+import react from "./react.svg";
+
 import "antd/dist/antd.css";
 import "./App.css";
 
@@ -10,26 +12,25 @@ class App extends Component {
     return (
       <>
         <div className="App">
-          <header className="App-header">
-            <p>
-              {"matteo manzinello"}
-              <br />
-              <span style={{ fontSize: "0.7em", lineHeight: "0.7em" }}>
-                {"26, computer engineer, freelance developer"}
-              </span>
-            </p>
-            <p>
-              <Icon type="instagram" theme="outlined" />
-              {"  "}
-              <Icon type="twitter" theme="outlined" />
-              {"  "}
-              <Icon type="facebook" theme="filled" />
-              {"  "}
-              <Icon type="github" theme="filled" />
-              {"  "}
-              <Icon type="mail" theme="outlined" />
-            </p>
-          </header>
+          <p>
+            matteo <strong>manzinello</strong>
+            <br />
+            <span style={{ fontSize: "0.7em", lineHeight: "0.7em" }}>
+              26, a <em>computer engineer</em>, a <em>freelance developer</em>
+            </span>
+          </p>
+          <p>
+            <Icon type="instagram" theme="outlined" />
+            {"  "}
+            <Icon type="twitter" theme="outlined" />
+            {"  "}
+            <Icon type="facebook" theme="filled" />
+            {"  "}
+            <Icon type="github" theme="filled" />
+            {"  "}
+            <Icon type="mail" theme="outlined" />
+          </p>
+          <img src={react} className="App-logo" alt="logo" />
         </div>
       </>
     );
