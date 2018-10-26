0a801c22cd6ea5a1e195809f2eb6d978b3525fc2
Matteo Manzinello <matteo@manzinello.com>
2018-10-26 17:01:40 +0200

    Bozza di sito web con react

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-import "./App.css";
-
 import { Helmet } from "react-helmet";
+import { Icon } from "antd";
+
+import "antd/dist/antd.css";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -10,9 +12,22 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <p>
-              matteo manzinello
+              {"matteo manzinello"}
               <br />
-              (work in progress)
+              <span style={{ fontSize: "0.7em", lineHeight: "0.7em" }}>
+                {"26, computer engineer, freelance developer"}
+              </span>
+            </p>
+            <p>
+              <Icon type="instagram" theme="outlined" />
+              {"  "}
+              <Icon type="twitter" theme="outlined" />
+              {"  "}
+              <Icon type="facebook" theme="filled" />
+              {"  "}
+              <Icon type="github" theme="filled" />
+              {"  "}
+              <Icon type="mail" theme="outlined" />
             </p>
           </header>
         </div>
