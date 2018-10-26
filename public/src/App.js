2065101ca4c824b19f7c0b462c35257580d3ca6f
Matteo Manzinello <matteo@manzinello.com>
2018-10-26 15:21:10 +0200

    Aggiunto helmet

--- public/src/App.js
+++ public/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import { Helmet } from "react-helmet";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            matteo manzinello
-            <br />
-            (work in progress)
-          </p>
-        </header>
-      </div>
+      <>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              matteo manzinello
+              <br />
+              (work in progress)
+            </p>
+          </header>
+        </div>
+      </>
     );
   }
 }
