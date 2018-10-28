32c0493fcd92efaad66f57c94dded5162e080d28
Matteo Manzinello <matteo@manzinello.com>
2018-10-28 10:32:01 +0100

    Sistemato IconLink

--- public/src/components/ManzinelloIconLink.js
+++ public/src/components/ManzinelloIconLink.js
@@ -6,7 +6,13 @@ class ManzinelloIconLink extends Component {
   render = () => {
     return (
       <>
-        <a className={"a-icon"} title={this.props.text} href={this.props.link}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className={"a-icon"}
+          title={this.props.text}
+          href={this.props.link}
+        >
           <Icon type={this.props.type} theme={this.props.theme} />
         </a>
       </>
