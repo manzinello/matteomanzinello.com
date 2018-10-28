1b770ad9d6c910f58ea346300db6d0ccbeb2eb01
Matteo Manzinello <matteo@manzinello.com>
2018-10-28 18:20:33 +0100

    Aggiunto react-router e le props nel ManzinelloIconLink

--- public/src/components/ManzinelloIconLink.js
+++ public/src/components/ManzinelloIconLink.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 import { Icon } from "antd";
 
@@ -6,18 +7,40 @@ class ManzinelloIconLink extends Component {
   render = () => {
     return (
       <>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          className={"a-icon"}
-          title={this.props.text}
-          href={this.props.link}
-        >
-          <Icon type={this.props.type} theme={this.props.theme} />
-        </a>
+        {!this.props.target ? (
+          <a
+            className={"a-icon"}
+            title={this.props.text}
+            href={this.props.link}
+          >
+            <Icon type={this.props.type} theme={this.props.theme} />
+          </a>
+        ) : (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className={"a-icon"}
+            title={this.props.text}
+            href={this.props.link}
+          >
+            <Icon type={this.props.type} theme={this.props.theme} />
+          </a>
+        )}
       </>
     );
   };
 }
+
+ManzinelloIconLink.propTypes = {
+  type: PropTypes.string,
+  theme: PropTypes.string,
+  text: PropTypes.string,
+  link: PropTypes.string,
+  target: PropTypes.bool
+};
+
+ManzinelloIconLink.defaultProps = {
+  target: true
+};
 
 export default ManzinelloIconLink;
