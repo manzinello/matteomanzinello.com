403703920c2a9a898549d40770d48ecf4edd1c5b
Matteo Manzinello <matteo@manzinello.com>
2019-01-16 17:08:18 +0100

    Rimossa icona da ManzinelloEmail

--- src/components/ManzinelloEmail.js
+++ src/components/ManzinelloEmail.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 
-import { Icon } from "rsuite";
-
 class ManzinelloEmail extends Component {
   render = () => {
     return (
       <>
         <code className={"manzinello-email"}>
-          <Icon icon={"send"} />{" "}
           <a href="mailto:matteo@manzinello.com">matteo@manzinello.com</a>
         </code>
       </>
