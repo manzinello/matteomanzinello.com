05977c19e046251794f9081424780d8398215448
Matteo Manzinello <matteo@manzinello.com>
2019-02-25 11:46:38 +0100

    Pulizia del codice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import ManzinelloIconLink from "./components/ManzinelloIconLink";
 import ManzinelloEmail from "./components/ManzinelloEmail";
