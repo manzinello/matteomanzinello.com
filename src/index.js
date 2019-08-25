64c6337e0acf8cf2300312e4d2f3e19f9311a827
Matteo Manzinello <matteo@manzinello.com>
2019-08-25 20:29:33 +0200

    sentry

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://103776ba2b304221bd538a9e88b95b9e@sentry.io/1541174"
+});
+
 ReactDOM.render(<App />, document.getElementById("root"));
