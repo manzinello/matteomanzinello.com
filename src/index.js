6ee5b4e35efbd11d7cd7399abecd22c0f801596b
Matteo Manzinello <matteo@manzinello.com>
2020-02-27 20:37:42 +0100

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import App from "./App";
 
 import * as Sentry from "@sentry/browser";
 
+import * as serviceWorker from "./serviceWorker";
+
 Sentry.init({
   dsn: "https://103776ba2b304221bd538a9e88b95b9e@sentry.io/1541174"
 });
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.register();
