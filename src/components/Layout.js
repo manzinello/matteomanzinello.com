f2260a9543e93f32302458979e9e58a51f41c963
Matteo Manzinello <matteo@manzinello.com>
2020-04-23 18:15:46 +0200

    Layout

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { initGA, logPageView } from "../utils/analytics";
+
+export default class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }
+
+  render() {
+    return <>{this.props.children}</>;
+  }
+}
