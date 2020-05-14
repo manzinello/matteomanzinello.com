141a987f7c7099263d8d38c2a49aedd63ab11cf1
Matteo Manzinello <matteo@manzinello.com>
2020-05-14 17:09:03 +0200

    pulizia di _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,9 @@ import React from "react";
 
 import { AppProps } from "next/app";
 
-// import Layout from "../components/Layout";
-
 import "../matteomanzinello.scss";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-
-  /*
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-  */
 }
