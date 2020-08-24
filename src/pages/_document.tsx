72fde7425640a1364a30e422f764f1036936b815
Matteo Manzinello <matteo@manzinello.com>
2020-08-24 10:26:05 +0200

    update dependencies!

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,10 +20,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
           <link
             rel="icon"
             type="image/png"
