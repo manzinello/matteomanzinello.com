4c4c7cd761aa4d81d42d424b9b0da95895937462
Matteo Manzinello <matteo@manzinello.com>
2021-02-22 17:20:49 +0100

    nuovo ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,22 +40,24 @@ class MyDocument extends Document {
           ></script>
 
           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-HW0EYFMV4B"
+          ></script>
+
+          <script
             dangerouslySetInnerHTML={{
-              __html: `(function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-T2H8JL3");`,
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'G-HW0EYFMV4B');
+                `,
             }}
           ></script>
           <meta
             name="description"
-            content="27, creative, engineer, developer"
+            content="28, creative, engineer, developer"
           />
         </Head>
         <body>
